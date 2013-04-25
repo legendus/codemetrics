2b31bafa1ed4ee9331032a377834909f7d4e8d18
Jose Evora <joseevora@siani.es>
2013-04-25 09:50:16 +0100

    First commit

---
+++ src/org/codemetric/Main.java
@@ -0,0 +1,45 @@
+package org.codemetric;
+
+import java.io.File;
+
+import org.sumus.dwh.datastore.DataStore;
+import org.sumus.dwh.datastore.DataStoreException;
+
+public class Main {
+
+	public static void main(String[] args) throws DataStoreException {
+		checkArgs(args);
+		DataStore dataStore = createDataStore(new File (args[1]));
+		MockParser parser = new MockParser(dataStore);
+		parser.execute(new File(args[0]));
+		dataStore.save();
+	}
+
+	private static DataStore createDataStore(File folder) {
+		DataStoreFactory factory = new DataStoreFactory(folder);
+		DataStore dataStore = factory.createDataStore();
+		return dataStore;
+	}
+
+	private static void checkArgs(String[] args) {
+		checkIsNotNull(args);
+		checkSourceExists(new File (args[0]));
+		checkTargetNotExists(new File (args[1]));
+	}
+
+	private static void checkSourceExists(File sourceDirectory) {
+		if(!sourceDirectory.exists())
+			throw new RuntimeException("Source path does not exist");
+	}
+	
+	private static void checkTargetNotExists(File targetDirectory) {
+		if(targetDirectory.exists())
+			throw new RuntimeException("Target path does not exist");
+	}
+
+	private static void checkIsNotNull(String[] args) {
+		if(args[0] == null || args[1] == null)
+			throw new RuntimeException("Paths not introduced");
+	}
+
+}
---
+++ src/org/codemetric/MockParser.java
@@ -0,0 +1,96 @@
+package org.codemetric;
+
+import java.io.File;
+import java.util.Date;
+
+import org.codemetric.definition.CubeDefinition;
+import org.codemetric.definition.DataStoreDefinition;
+import org.codemetric.definition.DimensionDefinition;
+import org.sumus.dwh.cube.Cube;
+import org.sumus.dwh.datastore.Context;
+import org.sumus.dwh.datastore.DataStore;
+import org.sumus.dwh.datastore.Entity;
+import org.sumus.dwh.datastore.State;
+import org.sumus.dwh.datastore.Tuple;
+import org.sumus.dwh.dimension.Dimension;
+
+public class MockParser {
+
+	private final DataStore dataStore;
+
+	public MockParser(DataStore dataStore) {
+		this.dataStore = dataStore;
+	}
+	
+	public DataStore getDataStore() {
+		return dataStore;
+	}
+
+	public void execute(File sourceFolder){
+		insertPackageEntity();
+		insertClassEntity();
+		insertMethodEntity();
+		insertPackageTuple();
+	}
+
+	private void insertPackageEntity() {
+		Entity component = new Entity("org.core", getDimension(DataStoreDefinition.MODULES));
+		component.addFeature(DataStoreDefinition.NAME.getName(), "org.core");
+		component.addFeature(DataStoreDefinition.TYPE.getName(), "Package");
+		add(component);
+	}
+	
+	private void insertClassEntity() {
+		Entity component = new Entity("org.core.Parser", getDimension(DataStoreDefinition.MODULES));
+		component.addFeature(DataStoreDefinition.NAME.getName(), "org.core.Parser");
+		component.addFeature(DataStoreDefinition.TYPE.getName(), "Class");
+		add(component);
+	}
+
+	private void insertMethodEntity() {
+		Entity component = new Entity("org.core.Parser.execute", getDimension(DataStoreDefinition.MODULES));
+		component.addFeature(DataStoreDefinition.NAME.getName(), "org.core.Parser.execute");
+		component.addFeature(DataStoreDefinition.TYPE.getName(), "Method");
+		add(component);
+	}
+	
+	private void insertPackageTuple() {
+		Context context = buildPackageContext();
+		State state = buildPackageState();
+		Tuple factShip = new Tuple(getCube(DataStoreDefinition.PACKAGE_CUBE), new Date(), context, state);
+		add(factShip);
+	}
+
+	private Context buildPackageContext() {
+		Context context = new Context ();
+		context.put(DataStoreDefinition.MODULES.getName(), "org.core");
+		return context;
+	}
+
+	private State buildPackageState() {
+		State state = new State();
+		state.put(DataStoreDefinition.LINES_OF_CODE.getName(), 5023.);
+		state.put(DataStoreDefinition.CLASSES.getName(), 5.);
+		state.put(DataStoreDefinition.EFFERENT_COUPLING_INTERNAL.getName(), 4.);
+		state.put(DataStoreDefinition.EFFERENT_COUPLING_LIBRARY.getName(), 2.);
+		state.put(DataStoreDefinition.AFFERENT_COUPLING.getName(), 4.);
+		return state;
+	}
+
+
+	private Dimension getDimension(DimensionDefinition definition) {
+		return getDataStore().getDimension(definition.getName());
+	}
+	
+	private Cube getCube(CubeDefinition definition) {
+		return getDataStore().getCube(definition.getName());
+	}
+	
+	private void add(Entity entity) {
+		getDataStore().insert(entity);
+	}
+	
+	private void add(Tuple tuple) {
+		getDataStore().insert(tuple);
+	}
+}
